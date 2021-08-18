ce94113c48d16c3b12390764b0b3a40dbf6c7931
Leftyxiv <manny@aledoux.net>
2021-08-18 06:22:07 -0400

    done for now

--- src/Map.ts
+++ src/Map.ts
@@ -29,7 +29,7 @@ export class Map {
     });
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: mappable.markerContent();
+        content: mappable.markerContent()
       });
       infoWindow.open(this.googleMap, marker);
     });
